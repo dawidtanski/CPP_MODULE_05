aa5a5a4b3286515ad1627f94e0d6a6e50d597e0f
DejVV <33099962+dawidtanski@users.noreply.github.com>
2025-10-02 13:22:55 +0200

    Add files via upload

--- ex01/inc/Form.hpp
+++ ex01/inc/Form.hpp
@@ -1,6 +1,20 @@
 #pragma once
 #include <iostream>
+#include <fstream>
 
 class Form{
 
+		private:
+			const std::string _name;
+			bool	_signed;
+			const int _gradeToSign;
+			const int _gradeToExec;
+
+		public:
+			Form();
+			~Form();
+			Form(const Form& other);
+			Form& operator=(const Form& other);
 };
+
+std::ostream& operator<<(std::ostream& os, Form& f);
